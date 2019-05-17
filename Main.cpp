415ba1e8b3e3fef7be9c3b734400e154938e7bdc
jiachenyang <34643052+jiachenyang@users.noreply.github.com>
2019-05-17 16:39:54 +0800

    服务器端应用功能最终版

---
+++ Main.cpp
@@ -0,0 +1,210 @@
+﻿#include"Vserver.h"
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<fstream>
+#pragma comment(lib,"libmysql.lib")
+using namespace std;
+
+DWORD WINAPI ServerThread(LPVOID lpParameter) {
+	SOCKET* ClientSocket = (SOCKET*)lpParameter;
+	Vserver v;
+	int receByt = 0;
+	char RecvBuf[1024];
+	char SendBuf[1024];
+	ofstream vfile("vlog.txt", ios::app);
+	while (1)
+	{
+		while (1)
+		{
+			receByt = recv(*ClientSocket, RecvBuf, sizeof(RecvBuf), 0);
+			if (receByt > 0) 
+			{
+				cout << "接收到的消息是：" << RecvBuf << "            来自客户端:" << *ClientSocket << endl;
+				break;
+			}
+		}
+		v.c2v = RecvBuf;
+		//判断是否OK
+		int f = v.function(v.c2v);//拆头部功能码，根据功能码返回int，0表示认证，1表示开始阅读，2表示下一页（根据文档功能码返回）
+		cout << "v.c2v:"<<v.c2v << endl;
+		switch (f)
+		{
+		case 0:   //认证
+		{
+			v.V_CDataDeEncapsulation(v.c2v);
+			if (v.Is_TrueClient(v.c2v)==true)
+			{
+				v.GetSign();
+				strcpy_s(SendBuf, v.V_CDataEncapsulation().c_str());//封装并且赋值给char数组SendBuf
+				memset(RecvBuf, 0, sizeof(RecvBuf));
+				int k = 0;
+				k = send(*ClientSocket, SendBuf, sizeof(SendBuf), 0);
+				if (k < 0) {
+					cout << "发送失败" << endl;
+				}
+				if (vfile.is_open())
+				{
+					vfile << v.V_TS() << " 收到来自" << v.ADC << "认证请求          认证成功！\n";
+					vfile.close();
+				}
+				memset(SendBuf, 0, sizeof(SendBuf)); 
+			}
+			else
+			{
+				strcpy_s(SendBuf, "认证失败，ticket与IDC不对应！");
+				memset(RecvBuf, 0, sizeof(RecvBuf));
+				int k = 0;
+				k = send(*ClientSocket, SendBuf, sizeof(SendBuf), 0);
+				if (k < 0) {
+					cout << "发送失败" << endl;
+				}
+				if (vfile.is_open())
+				{
+					vfile << v.V_TS() << " 收到来自" << v.ADC << "认证请求          认证失败！\n";
+					vfile.close();
+				}
+				memset(SendBuf, 0, sizeof(SendBuf));
+			}
+			break;
+		}
+		case 1:
+		{
+			v.V_CDataDeEnread(v.c2v);//解包得到bookname
+			v.page = 1;
+			if (v.getbook())//查找数据库，根据bookname，page 对content进行赋值
+			{
+				strcpy_s(SendBuf, v.content.c_str());//将content封装
+				memset(RecvBuf, 0, sizeof(RecvBuf));
+				int k = 0;
+				k = send(*ClientSocket, SendBuf, sizeof(SendBuf), 0);
+				if (k < 0) {
+					cout << "发送失败" << endl;
+				}
+				memset(SendBuf, 0, sizeof(SendBuf));
+			}
+			else
+			{
+				strcpy_s(SendBuf, "查无此书！");
+				v.page--;
+				memset(RecvBuf, 0, sizeof(RecvBuf));
+				int k = 0;
+				k = send(*ClientSocket, SendBuf, sizeof(SendBuf), 0);
+				if (k < 0) {
+					cout << "发送失败" << endl;
+				}
+				memset(SendBuf, 0, sizeof(SendBuf));
+			}
+			break;
+		}
+		case 2:
+		{
+			v.page++;
+			if (v.getbook())
+			{
+				strcpy_s(SendBuf, v.content.c_str());
+				memset(RecvBuf, 0, sizeof(RecvBuf));
+				int k = 0;
+				k = send(*ClientSocket, SendBuf, sizeof(SendBuf), 0);
+				if (k < 0) {
+					cout << "发送失败" << endl;
+				}
+				memset(SendBuf, 0, sizeof(SendBuf));
+			}
+			else
+			{
+				strcpy_s(SendBuf,"已经是最后一页了");
+				v.page--;
+				memset(RecvBuf, 0, sizeof(RecvBuf));
+				int k = 0;
+				k = send(*ClientSocket, SendBuf, sizeof(SendBuf), 0);
+				if (k < 0) {
+					cout << "发送失败" << endl;
+				}
+				memset(SendBuf, 0, sizeof(SendBuf));
+			}
+			break;
+		}
+		case 3:
+		{
+			v.page--;
+			if (v.getbook())
+			{
+				strcpy_s(SendBuf, v.content.c_str());
+				memset(RecvBuf, 0, sizeof(RecvBuf));
+				int k = 0;
+				k = send(*ClientSocket, SendBuf, sizeof(SendBuf), 0);
+				if (k < 0) {
+					cout << "发送失败" << endl;
+				}
+				memset(SendBuf, 0, sizeof(SendBuf));
+			}
+			else
+			{
+				strcpy_s(SendBuf, "已经是第一页了");
+				v.page++;
+				memset(RecvBuf, 0, sizeof(RecvBuf));
+				int k = 0;
+				k = send(*ClientSocket, SendBuf, sizeof(SendBuf), 0);
+				if (k < 0) {
+					cout << "发送失败" << endl;
+				}
+				memset(SendBuf, 0, sizeof(SendBuf));
+			}
+			break;
+		}
+		case 4: //充值功能，考虑RSA
+		{
+			break;
+		}
+		case 5:
+		{
+			strcpy_s(SendBuf,"已退出！");
+			memset(RecvBuf, 0, sizeof(RecvBuf));
+			int k = 0;
+			k = send(*ClientSocket, SendBuf, sizeof(SendBuf), 0);
+			if (k < 0) {
+				cout << "发送失败" << endl;
+			}
+			memset(SendBuf, 0, sizeof(SendBuf)); 
+			closesocket(*ClientSocket);
+			free(ClientSocket);
+			return 0;
+		}
+		}
+	}
+}
+
+
+int main()
+{
+	WSAData wsd;
+	WSAStartup(MAKEWORD(2, 2), &wsd);
+	SOCKET ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
+	SOCKADDR_IN ServerAddr;
+	ServerAddr.sin_family = AF_INET;
+	ServerAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+	ServerAddr.sin_port = htons(8022);
+	int n;
+	n = bind(ServerSocket, (struct sockaddr*)&ServerAddr, sizeof(ServerAddr));
+	if (n == SOCKET_ERROR) {
+		cout << "端口绑定失败！" << endl;
+		cout << WSAGetLastError() << endl;
+		return 0;
+	}
+	else {
+		cout << "端口绑定成功：" << 8022 << endl;
+	}
+	int l = listen(ServerSocket, 20);
+	cout << "服务端准备就绪，等待连接请求" << endl;
+	while (1)
+	{
+		SOCKET *ClientSocket = new SOCKET;
+		ClientSocket = (SOCKET*)malloc(sizeof(SOCKET));
+		int SockAddrlen = sizeof(sockaddr);
+		*ClientSocket = accept(ServerSocket, 0, 0);
+		cout << "一个客户端已连接，socket是：" << *ClientSocket << endl;
+		CreateThread(NULL, 0, &ServerThread, ClientSocket, 0, NULL);
+	}
+	return 0;
+}
---
+++ Vserver.cpp
@@ -0,0 +1,191 @@
+﻿#include "Vserver.h"
+#include "des.h"
+#include <mysql.h>
+#include <iostream>
+#include<functional>
+//获取当前时间戳
+string Vserver::V_TS()
+{
+	time_t now;
+	struct tm pt;
+	time(&now);
+	localtime_s(&pt, &now);
+	string ts;
+	string year, mon, day, hour, min, sec;
+	year = to_string(pt.tm_year - 100);
+	if (year.length() == 1)
+	{
+		year = "0" + year;
+	}
+	mon = to_string(pt.tm_mon + 1);
+	if (mon.length() == 1)
+	{
+		mon = "0" + mon;
+	}
+	day = to_string(pt.tm_mday);
+	if (day.length() == 1)
+	{
+		day = "0" + day;
+	}
+	hour = to_string(pt.tm_hour);
+	if (hour.length() == 1)
+	{
+		hour = "0" + hour;
+	}
+	min = to_string(pt.tm_min);
+	if (min.length() == 1)
+	{
+		min = "0" + min;
+	}
+	sec = to_string(pt.tm_sec);
+	if (sec.length() == 1)
+	{
+		sec = "0" + sec;
+	}
+	ts = year + mon + day + hour + min + sec;
+	return ts;
+}
+
+//对最终要发回Client的数据包进行封装加密	keycv(ts5+"0000")+"signdata"+"sign"
+string Vserver::V_CDataEncapsulation()
+{
+	string v2c;
+	v2c += ts5;
+	v2c += "0000";
+	string a;
+	string b;
+	for (int i = 0; i < 2; i++)
+	{
+		a.assign(v2c, 0 + 8 * i, 8);
+		b += jiami(a, KeyCV);
+	}
+	string last;
+	last += b;
+	last += signdata;
+	last += sign;
+	cout << "ts5:"<<ts5 << endl;
+	return last;
+}
+
+//将Client发来的数据包进行解封装
+void Vserver::V_CDataDeEncapsulation(string data)
+{
+	string temp;
+	temp.assign(data,49,32);
+	string a;
+	string b;
+	for (int i = 0; i < 4; i++)
+	{
+		a.assign(temp, 0 + 8 * i, 8);
+		b += jiemi(a, KeyCV);
+	}
+	IDC.assign(b,0,4);
+	ts5.assign(b,19,12);
+	string t1;
+	t1.assign(ts5,0,6);
+	string t2;
+	t2.assign(ts5,6,6);
+	int ts;
+	ts = atoi(t2.c_str());
+	ts++;
+	string v = to_string(ts);
+	while (v.length() < 6)
+		v = "0" + v;
+	ts5 = t1+ v;
+}
+
+int Vserver::function(string data)
+{
+	int choice;
+	string a;
+	a.assign(data, 0, 1);
+	choice = atoi(a.c_str());
+	cout << "choice:" << choice << endl;
+	return choice;
+}
+
+bool Vserver::Is_TrueClient(string data)
+{
+	string a;
+	string b;
+	string temp;
+	temp.assign(data,1,48);
+	for (int i = 0; i < 6; i++)
+	{
+		a.assign(temp, 0 + 8 * i, 8);
+		b += jiemi(a, KeyCV);
+	}
+	string tempidc;
+	tempidc.assign(b, 8, 4);
+	cout << "tempidc:"<<tempidc <<"  IDC:"<< IDC << endl;
+	if (tempidc == IDC)
+		return true;
+	else
+		return false;
+}
+
+bool Vserver::getbook()
+{
+	MYSQL* mysql = new MYSQL;
+	MYSQL_FIELD* fd;
+	char field[32][32];
+	MYSQL_RES* res;
+	MYSQL_ROW column;
+	char query[150];
+	mysql_init(mysql);
+	mysql_options(mysql, MYSQL_SET_CHARSET_NAME, "gbk");
+	if (!(mysql_real_connect(mysql, "127.0.0.1", "root", "jcy826089", "vserver", 3306, NULL, 0)))
+	{
+		cout << "ERROR" << endl;
+		return false;
+	}
+	else
+	{
+		cout << "connect successfully" << endl;
+	}
+	sprintf_s(query, "SELECT content from book where bookname='%s' and page=%d", bookname.c_str(), page);
+	if (mysql_query(mysql, query))
+	{
+		cout << mysql_error(mysql) << endl;
+	}
+	else
+	{
+		if (!(res = mysql_store_result(mysql)))
+		{
+			cout << "errrr" << endl;
+			return false;
+		}
+		else
+		{
+			column = mysql_fetch_row(res);
+			cout << column[0] << endl;
+			content = column[0];
+		}
+	}
+	return true;
+}
+
+void Vserver::V_CDataDeEnread(string data)
+{
+	bookname.assign(data, 1, data.length() - 1);
+}
+
+void Vserver::GetSign()
+{
+	rsa.d = (BigInteger)"2A76B2E3A91281A47014D981";
+	rsa.n = (BigInteger)"2C2A4E780336C5FCE454959D";
+	hash<string> hash_str;
+	cout << hash_str(signdata) << endl;
+	//long ii = hash_str(signdata);
+	//cout << ii;
+	char qqq[100];
+	itoa(hash_str(signdata), qqq, 10);
+	string str;
+	str.assign(qqq);
+	cout << "str:"<<str << endl;
+	str.assign(str, 1, 10);
+	BigInteger m1(str);
+	BigInteger c = rsa.encryptByPrivate(m1);
+	sign = c.toString();
+	cout << "sign:" << sign << " signdata:" << signdata << endl;
+}
